00ca38fa9a9db95f1f6ad56707ae1df143780545
vinutha <vinuthad.52@gmail.com>
2019-07-08 10:56:15 +0000

    reverse.c modified again

--- reverse.c
+++ reverse.c
@@ -1,16 +1,21 @@
-#include <stdio.h>
-#include <string.h>
- 
+#include<stdio.h>
 void reverse()
 {
-   char arr[100];
- 
-   printf("Enter a string to reverse\n");
-   gets(arr);
- 
-   strrev(arr);
- 
-   printf("Reverse of the string is \n%s\n", arr);
- 
-   
+    int i, j, k;
+    char str[100];
+    char rev[100];
+    printf("Enter a string:\t");
+    scanf("%s", str);
+    printf("The original string is %s\n", str);
+    for(i = 0; str[i] != '\0'; i++);
+    {
+        k = i-1;
+    }
+    for(j = 0; j <= i-1; j++)
+    {
+        rev[j] = str[k];
+        k--;
+    }
+    printf("The reverse string is %s\n", rev);
+    
 }
